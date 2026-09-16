b7035b23a68c0dda7efdc415766b0e86d0de6d9c
Will Taylor <w1ll100@hotmail.co.uk>
2026-09-16 21:26:42 +0100

    Make it possible to specify draft format from CLI

--- bin/draft_record.py
+++ bin/draft_record.py
@@ -6,34 +6,44 @@ Relies on draft files (screenshots) being stored in
 /home/will/Pictures/Screenshots/drafts, with filenames ending '{W}-{L}.{ext}'
 where {W} and {L} are the number of wins/losses achieved.
 
-Doesn't currently handle nested directories.
+Gets drafts from the most recently updated subdirectory if no argument provided.
+
+Provide an argument to get that specific directory.
 """
 
 import os
+import sys
 from pathlib import Path
 
 drafts_home = Path("/home/will/Pictures/Screenshots/drafts")
 
-def get_most_recent_folder():
-    dirs_of_interest = []
-    for path, dirs, files in os.walk(drafts_home):
-        if not dirs:
-            dirs_of_interest.append(drafts_home / path)
+def get_draft_dir():
+    def get_most_recent_dir(root):
+        dirs_of_interest = []
+        for path, dirs, files in os.walk(root):
+            if not dirs:
+                dirs_of_interest.append(drafts_home / path)
 
-    return max(dirs_of_interest, key=lambda f: f.stat().st_mtime)
+        return max(dirs_of_interest, key=lambda f: f.stat().st_mtime)
 
-most_recent = get_most_recent_folder()
+    if len(sys.argv) <= 1:
+        return get_most_recent_dir(drafts_home)
+    else:
+        d = drafts_home / sys.argv[1]
+        _, dirs, fs = next(os.walk(d))
+        if dirs:
+            return get_most_recent_dir(d)
+        else:
+            return d
 
-bits = [most_recent.name]
-p = most_recent.parent
-while p != drafts_home:
-    bits.append(p.name)
-    p = p.parent
+
+draft_dir = get_draft_dir()
+
 # The bit of the path starting at drafts_home
-partial_path = "/".join(reversed(bits))
+p = draft_dir.parts[len(drafts_home.parts):]
 
-fs = os.listdir(most_recent)
-print(f"{partial_path}: {len(fs)} drafts")
+fs = os.listdir(draft_dir)
+print(f"{"/".join(p)}: {len(fs)} drafts")
 
 no_ext = (f.split(".")[0] for f in fs)
 splits = (f.split("-") for f in no_ext)
